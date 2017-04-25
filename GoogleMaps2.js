6eae2196fae730a97b78dc591d1a22a5b19d820f
Seth Jordan Connell <sethjordanconnell@gmail.com>
2017-04-25 00:39:27 -0500

    Updated Some Files
    I can’t remember what I did. Lol.

--- GoogleMaps2.js
+++ GoogleMaps2.js
@@ -1,230 +1,206 @@
-var map;
-var places = [{
-    name: "Old Home",
-    windowtext: "I used to live there.",
-    cords: {
-        lat: 35.626017,
-        lng: -92.245208
-    }}, 
-    {
-    name: "Current Home",
-    windowtext: "I currently live here.",
-    cords: {
-        lat: 35.627849,
-        lng: -92.239511
-    }}, {
-    name: "Mexican Restraunt",
-    windowtext: "This is a mexican restruant I love to eat at.",
-    cords: {
-        lat: 35.605847,
-        lng: -92.269067
-    }}, {
-    name: "Post Office",
-    windowtext: "This is the post office I recieve my mail at.",
-    cords: {
-        lat: 35.6106,
-        lng: -92.262671
-    }}, {
-    name: "Bank",
-    windowtext: "This is the bank I earn interest at.",
-    cords: {
-        lat: 35.61135,
-        lng: -92.262556
-}}];
+var map,
+    createMarker,
+    marker0,
+    marker1,
+    marker2,
+    marker3,
+    marker4,
+    markerarray;
 
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: places[2].cords,
-        zoom: 13,
-        styles: [{
-                elementType: 'geometry',
-                stylers: [{
-                    color: '#242f3e'
-                }]
-            },
-            {
-                elementType: 'labels.text.stroke',
-                stylers: [{
-                    color: '#242f3e'
-                }]
-            },
-            {
-                elementType: 'labels.text.fill',
-                stylers: [{
-                    color: '#746855'
-                }]
-            },
-            {
-                featureType: 'administrative.locality',
-                elementType: 'labels.text.fill',
-                stylers: [{
-                    color: '#d59563'
-                }]
-            },
-            {
-                featureType: 'poi',
-                elementType: 'labels.text.fill',
-                stylers: [{
-                    color: '#d59563'
-                }]
-            },
-            {
-                featureType: 'poi.park',
-                elementType: 'geometry',
-                stylers: [{
-                    color: '#263c3f'
-                }]
-            },
-            {
-                featureType: 'poi.park',
-                elementType: 'labels.text.fill',
-                stylers: [{
-                    color: '#6b9a76'
-                }]
-            },
-            {
-                featureType: 'road',
-                elementType: 'geometry',
-                stylers: [{
-                    color: '#38414e'
-                }]
-            },
-            {
-                featureType: 'road',
-                elementType: 'geometry.stroke',
-                stylers: [{
-                    color: '#212a37'
-                }]
-            },
-            {
-                featureType: 'road',
-                elementType: 'labels.text.fill',
-                stylers: [{
-                    color: '#9ca5b3'
-                }]
-            },
-            {
-                featureType: 'road.highway',
-                elementType: 'geometry',
-                stylers: [{
-                    color: '#746855'
-                }]
-            },
-            {
-                featureType: 'road.highway',
-                elementType: 'geometry.stroke',
-                stylers: [{
-                    color: '#1f2835'
-                }]
-            },
-            {
-                featureType: 'road.highway',
-                elementType: 'labels.text.fill',
-                stylers: [{
-                    color: '#f3d19c'
-                }]
-            },
-            {
-                featureType: 'transit',
-                elementType: 'geometry',
-                stylers: [{
-                    color: '#2f3948'
-                }]
-            },
-            {
-                featureType: 'transit.station',
-                elementType: 'labels.text.fill',
-                stylers: [{
-                    color: '#d59563'
-                }]
-            },
-            {
-                featureType: 'water',
-                elementType: 'geometry',
-                stylers: [{
-                    color: '#17263c'
-                }]
-            },
-            {
-                featureType: 'water',
-                elementType: 'labels.text.fill',
-                stylers: [{
-                    color: '#515c6d'
-                }]
-            },
-            {
-                featureType: 'water',
-                elementType: 'labels.text.stroke',
-                stylers: [{
-                    color: '#17263c'
-                }]
-            }
-        ]
-    });
-    var stringstring =
-        '<br>' +
-        "Also, here's a cute picture of a cat." +
-        '<br>' +
-        '<img id = "catpic" src="http://thecatapi.com/api/images/get?format=src&type=gif" alt="Sorry, this image is not available.">'
-        
-        
+var initMap = function() {
+    try {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: model.places[2].cords,
+            zoom: 13,
+            styles: [{
+                    elementType: 'geometry',
+                    stylers: [{
+                        color: '#242f3e'
+                    }]
+                },
+                {
+                    elementType: 'labels.text.stroke',
+                    stylers: [{
+                        color: '#242f3e'
+                    }]
+                },
+                {
+                    elementType: 'labels.text.fill',
+                    stylers: [{
+                        color: '#746855'
+                    }]
+                },
+                {
+                    featureType: 'administrative.locality',
+                    elementType: 'labels.text.fill',
+                    stylers: [{
+                        color: '#d59563'
+                    }]
+                },
+                {
+                    featureType: 'poi',
+                    elementType: 'labels.text.fill',
+                    stylers: [{
+                        color: '#d59563'
+                    }]
+                },
+                {
+                    featureType: 'poi.park',
+                    elementType: 'geometry',
+                    stylers: [{
+                        color: '#263c3f'
+                    }]
+                },
+                {
+                    featureType: 'poi.park',
+                    elementType: 'labels.text.fill',
+                    stylers: [{
+                        color: '#6b9a76'
+                    }]
+                },
+                {
+                    featureType: 'road',
+                    elementType: 'geometry',
+                    stylers: [{
+                        color: '#38414e'
+                    }]
+                },
+                {
+                    featureType: 'road',
+                    elementType: 'geometry.stroke',
+                    stylers: [{
+                        color: '#212a37'
+                    }]
+                },
+                {
+                    featureType: 'road',
+                    elementType: 'labels.text.fill',
+                    stylers: [{
+                        color: '#9ca5b3'
+                    }]
+                },
+                {
+                    featureType: 'road.highway',
+                    elementType: 'geometry',
+                    stylers: [{
+                        color: '#746855'
+                    }]
+                },
+                {
+                    featureType: 'road.highway',
+                    elementType: 'geometry.stroke',
+                    stylers: [{
+                        color: '#1f2835'
+                    }]
+                },
+                {
+                    featureType: 'road.highway',
+                    elementType: 'labels.text.fill',
+                    stylers: [{
+                        color: '#f3d19c'
+                    }]
+                },
+                {
+                    featureType: 'transit',
+                    elementType: 'geometry',
+                    stylers: [{
+                        color: '#2f3948'
+                    }]
+                },
+                {
+                    featureType: 'transit.station',
+                    elementType: 'labels.text.fill',
+                    stylers: [{
+                        color: '#d59563'
+                    }]
+                },
+                {
+                    featureType: 'water',
+                    elementType: 'geometry',
+                    stylers: [{
+                        color: '#17263c'
+                    }]
+                },
+                {
+                    featureType: 'water',
+                    elementType: 'labels.text.fill',
+                    stylers: [{
+                        color: '#515c6d'
+                    }]
+                },
+                {
+                    featureType: 'water',
+                    elementType: 'labels.text.stroke',
+                    stylers: [{
+                        color: '#17263c'
+                    }]
+                }
+            ]
+        });
+        var moreInfo = function(state) {
+            var string = '<br>' + "(This location is in " + state + ")" + '<br>' + '<img id = "catpic" src="http://connell.systems/StateMapsAPI/' + state.toLowerCase() +'.jpg" alt="Sorry, this image is not available.">'
+            return string;
+        };
 
-    
-    
-    var createMarker = function(cord) {
-        return new google.maps.Marker({
-            position: cord,
-            map: map,
-            title: 'Hello World!',
-            icon: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
+        createMarker = function(cord) {
+            return new google.maps.Marker({
+                position: cord,
+                map: map,
+                title: 'Hello World!',
+                icon: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
+            })
+        };
+
+        marker0 = createMarker(model.places[0].cords);
+        marker1 = createMarker(model.places[1].cords);
+        marker2 = createMarker(model.places[2].cords);
+        marker3 = createMarker(model.places[3].cords);
+        marker4 = createMarker(model.places[4].cords);
+
+        markerarray = [marker0, marker1, marker2, marker3, marker4];
+
+        marker0.addListener("click", function(){
+                marker0.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
+                var infowindow = new google.maps.InfoWindow({
+
+                    content: '<h1>' + model.places[0].name + '</h1>' + '<br>' + '<h3>' + model.places[0].windowtext + '</h3>' + moreInfo(model.places[0].state)
+                });
+                infowindow.open(map, marker0);
+            });
+
+        marker1.addListener("click", function(){
+                marker1.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
+                var infowindow = new google.maps.InfoWindow({
+                    content: '<h1>' + model.places[1].name + '</h1>' + '<br>' + '<h3>' + model.places[1].windowtext + '</h3>' + moreInfo(model.places[1].state)
+                });
+                infowindow.open(map, marker1);
+            });
+
+        marker2.addListener("click", function(){
+                marker2.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
+                var infowindow = new google.maps.InfoWindow({
+                    content: '<h1>' + model.places[2].name + '</h1>' + '<br>' + '<h3>' + model.places[2].windowtext + '</h3>' + moreInfo(model.places[2].state)
+                });
+                infowindow.open(map, marker2);
+            });
+
+        marker3.addListener("click", function(){
+                marker3.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
+                var infowindow = new google.maps.InfoWindow({
+                    content: '<h1>' + model.places[3].name + '</h1>' + '<br>' + '<h3>' + model.places[3].windowtext + '</h3>' + moreInfo(model.places[3].state)
+                });
+                infowindow.open(map, marker3);
+            });
+
+        marker4.addListener("click", function(){
+                marker4.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
+                var infowindow = new google.maps.InfoWindow({
+                    content: '<h1>' + model.places[4].name + '</h1>' + '<br>' + '<h3>' + model.places[4].windowtext + '</h3>' + moreInfo(model.places[4].state)
+                });
+                infowindow.open(map, marker4);
         });
     }
-    
-    var marker0 = createMarker(places[0].cords);
-    var marker1 = createMarker(places[1].cords);
-    var marker2 = createMarker(places[2].cords);
-    var marker3 = createMarker(places[3].cords);
-    var marker4 = createMarker(places[4].cords);
-    
-    var markerarray = [marker0, marker1, marker2, marker3, marker4];
-    
-    marker1.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png')
-    
-    marker0.addListener("click", function(){
-            var infowindow = new google.maps.InfoWindow({
-                
-                content: '<br>' + '<h1>' + places[0].name + '</h1>' + '<br>' + '<h3>' + places[0].windowtext + '</h3>' + stringstring
-            });
-            infowindow.open(map, marker0);
-        });
-    
-    marker1.addListener("click", function(){
-            var infowindow = new google.maps.InfoWindow({
-                content: '<br>' + '<h1>' + places[1].name + '</h1>' + '<br>' + '<h3>' + places[1].windowtext + '</h3>' + stringstring
-            });
-            infowindow.open(map, marker1);
-        });
-    
-    marker2.addListener("click", function(){
-            var infowindow = new google.maps.InfoWindow({
-                content: '<br>' + '<h1>' + places[2].name + '</h1>' + '<br>' + '<h3>' + places[2].windowtext + '</h3>' + stringstring
-            });
-            infowindow.open(map, marker2);
-        });
-    
-    marker3.addListener("click", function(){
-            var infowindow = new google.maps.InfoWindow({
-                content: '<br>' + '<h1>' + places[3].name + '</h1>' + '<br>' + '<h3>' + places[3].windowtext + '</h3>' + stringstring
-            });
-            infowindow.open(map, marker3);
-        });
-    
-    marker4.addListener("click", function(){
-            var infowindow = new google.maps.InfoWindow({
-                content: '<br>' + '<h1>' + places[4].name + '</h1>' + '<br>' + '<h3>' + places[4].windowtext + '</h3>' + stringstring
-            });
-            infowindow.open(map, marker4);
-        });
-    
-    
+    catch(err) {
+        alert("Crap, there was an error: " + err);
+    }
 };
--- helper.js
+++ helper.js
@@ -1,6 +1,93 @@
 var ViewModel = {
-    monkey: ko.observable(),
-    
+    searchterm: ko.observable()
 };
-ViewModel.monkey("Created by Seth J. Connell");
+
+var model = {
+    init: function() {
+        for (var i = 0; i < 5; i++) {
+            var names = String(this.places[i].name);
+            $("#results").append("<li id ='" + i + "'" + "class = 'results'>" + names + "</li>");
+        }
+        this.bindClicks();
+    },
+    places : [{
+        state: "arkansas",
+        name: "Old Home",
+        windowtext: "I used to live there.",
+        cords: {
+            lat: 35.626017,
+            lng: -92.245208
+        }}, 
+        {
+        state: "arkansas",
+        name: "Current Home",
+        windowtext: "I currently live here.",
+        cords: {
+            lat: 35.627849,
+            lng: -92.239511
+        }}, {
+        name: "Mexican Restraunt",
+        state: "arkansas",
+        windowtext: "This is a mexican restruant I love to eat at.",
+        cords: {
+            lat: 35.605847,
+            lng: -92.269067
+        }}, {
+        name: "Post Office",
+        state: "Arkansas",
+        windowtext: "This is the post office I recieve my mail at.",
+        cords: {
+            lat: 35.6106,
+            lng: -92.262671
+        }}, {
+        name: "Bank",
+        state: "arkansas",
+        windowtext: "This is the bank I earn interest at.",
+        cords: {
+            lat: 35.61135,
+            lng: -92.262556
+        }}],
+    bindClicks: function() {
+        $("#0").click(function() {
+            markerarray[0].setIcon('http://maps.google.com/mapfiles/ms/icons/pink-dot.png');
+        })
+        $("#1").click(function() {
+            markerarray[1].setIcon('http://maps.google.com/mapfiles/ms/icons/pink-dot.png');
+        })
+        $("#2").click(function() {
+            markerarray[2].setIcon('http://maps.google.com/mapfiles/ms/icons/pink-dot.png');
+        })
+        $("#3").click(function() {
+            markerarray[3].setIcon('http://maps.google.com/mapfiles/ms/icons/pink-dot.png');
+        })
+        $("#4").click(function() {
+            markerarray[4].setIcon('http://maps.google.com/mapfiles/ms/icons/pink-dot.png');
+        })
+    },
+    searchitem: function(x) {
+        this.reset();
+        for (var i = 0; i < markerarray.length; i++) {
+            var names = String(this.places[i].name);
+            var lowercasenames = names.toLowerCase();
+            if (lowercasenames.includes(x.toLowerCase())) {
+                $("#results").append("<li id ='" + i + "'" + "class = 'results'>" + names + "</li>");
+            }
+        }
+        this.bindClicks();
+    },
+    filter: function() {
+        model.searchitem(ViewModel.searchterm())
+    },
+    searcharray: function(x) {
+        ViewModel.listResults = ko.observable(x);
+    },
+    reset: function() {
+        for (var i = 0; i < markerarray.length; i++) {
+            markerarray[i].setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
+            $("#results").empty();
+        }
+    }
+};
+
 ko.applyBindings(ViewModel);
+model.init();
