760327153b79f60b6452907f1c49e4145b9e24be
Seth J. Connell <sethjordanconnell@gmail.com>
2017-05-16 13:28:09 -0500

    Got Filter to Work (Barely)

--- helper.js
+++ helper.js
@@ -1,53 +1,6 @@
-var ViewModel = {
-    searchterm: ko.observable(), // searchterm is an observable object that saves the text from the searchbox
-    variable: ko.observable(),
-    abstractInfo: ko.observable("Nothing."),
-    clickedItem: function(x) { // This function creates 'if clicked...' instances binding the clicks of the list items to animation effects for markers. 
-        view.resetIcons();
-        view.setIcon(markerarray, x, 'http://maps.google.com/mapfiles/ms/icons/pink-dot.png');
-        if (x == 0) {
-            view.resetIcons();
-            marker0SetWindow();
-        };
-        if (x == 1) {
-            view.resetIcons();
-            marker1SetWindow();
-        };
-        if (x == 2) {
-            view.resetIcons();
-            marker2SetWindow();
-        };
-        if (x == 3) {
-            view.resetIcons();
-            marker3SetWindow();
-        };
-        if (x == 4) {
-            view.resetIcons();
-            marker4SetWindow();  
-        };
-    },
-    searchitem: function(x) { // This function resets the list, going through and appending each item to the list area of the page.
-        this.reset();
-        for (var i = 0; i < markerarray.length; i++) {
-            var names = String(this.places[i].name);
-            var lowercasenames = names.toLowerCase();
-            if (lowercasenames.includes(x.toLowerCase())) {
-                view.appendContent("#results", "<li id ='" + i + "'" + "class = 'results'>" + names + "</li>");
-            }
-        }
-    },
-    filter: function() { // this function searches the search term.
-        ViewModel.searchitem(ViewModel.searchterm())
-    },
-    reset: function() { // This resets the search results (it also clears the default items in the resets list).
-        for (var i = 0; i < markerarray.length; i++) {
-            view.setIcon(markerarray, i, 'http://maps.google.com/mapfiles/ms/icons/red-dot.png');
-            view.emptyElement("#results");
-        }
-    }
-};
-
 var model = {
+    names2: ["Old Home", "Current Home", "Mexican Restraunt", "Post Office", "Bank"],
+    names: ["Old Home", "Current Home", "Mexican Restraunt", "Post Office", "Bank"],
     places: [{ // Places is an arrray of location info. It's referenced by searchitem function.
             state: "arkansas",
             name: "Old Home",
@@ -94,6 +47,9 @@ var model = {
 };
 
 var view = { // The view is a list of all the functions that change the look of the page.
+    appendContent: function(element, content) { // this is used to append content to certain elements with specific ids, tags, or classes
+        $(element).append(content)
+    },
     emptyElement: function(element) { // this function empties the items in an element.
         $(element).empty();
     },
@@ -107,6 +63,66 @@ var view = { // The view is a list of all the functions that change the look of 
     },
     handleError: function() {
         alert("Sorry. Google Maps is not available.");
+    }
+};
+
+var ViewModel = {
+    names: ko.observableArray(model.names),
+    location: ko.observableArray(model.places),
+    query: ko.observable(""), // Query represents the value of the search box
+    searchterm: ko.observable(""), // searchterm is an observable object that saves the text from the searchbox
+    variable: ko.observable(),
+    abstractInfo: ko.observable("Nothing."),
+    clickedItem: function (x) { // This function creates 'if clicked...' instances binding the clicks of the list items to animation effects for markers. 
+        view.resetIcons();
+        if (x == "Old Home") {
+            view.resetIcons();
+            view.setIcon(markerarray, 0, 'http://maps.google.com/mapfiles/ms/icons/pink-dot.png');
+            marker0SetWindow();
+        } 
+        if (x == "Current Home") {
+            view.resetIcons();
+            view.setIcon(markerarray, 1, 'http://maps.google.com/mapfiles/ms/icons/pink-dot.png');
+            marker1SetWindow();
+        }
+        if (x == "Mexican Restraunt") {
+            view.resetIcons();
+            view.setIcon(markerarray, 2, 'http://maps.google.com/mapfiles/ms/icons/pink-dot.png');
+            marker2SetWindow();
+        }
+        if (x == "Post Office") {
+            view.resetIcons();
+            view.setIcon(markerarray, 3, 'http://maps.google.com/mapfiles/ms/icons/pink-dot.png');
+            marker3SetWindow();
+        }
+        if (x == "Bank") {
+            view.resetIcons();
+            view.setIcon(markerarray, 4, 'http://maps.google.com/mapfiles/ms/icons/pink-dot.png');
+            marker4SetWindow();  
+        }
+    },
+    searchitem: function(x) { // This function resets the list, going through and appending each item to the list area of the page.
+        this.reset();
+        for (var i = 0; i < markerarray.length; i++) {
+            var names = String(model.places[i].name);
+            var lowercasenames = names.toLowerCase();
+            if (lowercasenames.includes(x.toLowerCase()) && (ViewModel.names().includes() != true)) {
+            } else {
+                ViewModel.names.remove(names);
+            }
+        }
+    },
+    search: function(value) {
+        // remove all the current beers, which removes them from the view
+        console.log(value);
+    },
+    filter: function() { // this function searches the search term.
+        ViewModel.searchitem(ViewModel.searchterm());
+    },
+    reset: function() { // This resets the colors of the markers
+        for (var i = 0; i < markerarray.length; i++) {
+            view.setIcon(markerarray, i, 'http://maps.google.com/mapfiles/ms/icons/red-dot.png');
+        }
     }
 };
 ViewModel.searchterm.subscribe(ViewModel.filter);
